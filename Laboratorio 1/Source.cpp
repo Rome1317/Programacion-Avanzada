1ead3160ef5aee05754542e093631fb3f50ffd3e
Rome Glez <rome_gs@hotmail.com>
2019-02-09 17:01:11 -0600

    Punteros

---
+++ Laboratorio 1/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	
+	int imp[10];
+	int *pointer;
+
+	for (int i = 0; i < 10; i++){
+
+		imp[i] = 2 * i + 1;
+
+	}
+
+	for (int z = 0; z < 2; z++) {
+
+		pointer = imp;
+
+		for (int i = 0; i < 10; i++) {
+
+
+			cout << *pointer << " " << pointer << endl;
+			pointer++;
+
+		}
+
+		cout << endl;
+
+	}
+
+	system("pause>nul");
+
+}
