76e6396c5c68f96344ea1f202a8d39eb28bc9c6f
Rome Glez <rome_gs@hotmail.com>
2019-04-30 21:27:29 -0500

    Progra

---
+++ Proyectos/P. Avanzada/Final x2/resource.h
@@ -0,0 +1,63 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Final.rc
+//
+#define IDD_INFO                        101
+#define IDD_NEWDATE                     103
+#define IDD_AGENDA                      105
+#define IDD_EDITAR                      107
+#define IDD_ELIMINAR                    109
+#define IDR_MENU1                       112
+#define IDD_LOGIN                       113
+#define IDD_SINGUP                      115
+#define IDD_PRINCIPAL                   117
+#define IDC_STATIC7                     1001
+#define IDC_COMBO1                      1002
+#define IDC_STATIC2                     1002
+#define IDC_EDIT1                       1003
+#define IDC_EDIT2                       1004
+#define IDC_EDIT5                       1005
+#define IDC_DATETIMEPICKER1             1005
+#define IDC_EDIT8                       1006
+#define IDC_COMBO2                      1006
+#define IDC_STATIC3                     1006
+#define IDC_EDIT9                       1007
+#define IDC_EDIT3                       1007
+#define IDC_STATIC5                     1007
+#define IDC_EDIT4                       1008
+#define IDC_MONTHCALENDAR1              1010
+#define IDC_VET                         1010
+#define IDC_DATE                        1011
+#define IDC_EDIT6                       1012
+#define IDC_AGENDA                      1012
+#define IDC_EDIT7                       1013
+#define IDC_LIST3                       1021
+#define IDC_BUTTON1                     1024
+#define IDC_BUTTON2                     1025
+#define IDC_BUTTON3                     1026
+#define IDC_BUTTON4                     1027
+#define IDC_EXIT                        1027
+#define IDC_CHECK1                      1028
+#define IDC_CHECK2                      1029
+#define IDC_DATETIMEPICKER2             1030
+#define ID_AGENDA_VER                   40011
+#define ID_CITA_NUEVA                   40012
+#define ID_CITA_EDITAR                  40013
+#define ID_CITA_ELIMINAR                40014
+#define ID_VETERINARIO_INFORMACION      40015
+#define ID_VETERINARIO_EDITAR           40016
+#define ID_ARCHIVO_NUEVO                40017
+#define ID_ARCHIVO_ABRIR                40018
+#define ID_ARCHIVO_GUARDAR              40019
+#define ID_ARCHIVO_SALIR                40020
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        119
+#define _APS_NEXT_COMMAND_VALUE         40021
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
